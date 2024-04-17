7442083fdb9fa163c30e7e867b5a34bf0944fc5d
Jianan <qinxiandiqi@foxmail.com>
2024-04-17 23:43:02 +0800

    refactor:简化实现链构造

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+from llm_chain import LLMChain
 from agent_llm_chain import AgentLLMChain
 from retrieval_llm_chain import RetrievalLLMChain
 from simple_llm_chain import SimpleLlmChain
 
 if __name__ == "__main__":
+
+    def create_chain(choice) -> LLMChain:
+        if choice == "1":
+            return SimpleLlmChain()
+        elif choice == "2":
+            return RetrievalLLMChain()
+        elif choice == "3":
+            return AgentLLMChain()
+        else:
+            print("无效的选项，请重新选择。")
+            return None
+        
     while True:
         print("请选择模型类型：")
         print("1. 简单模型")
         print("2. 检索模型")
         print("3. 代理模型")
         choice = input("请输入选项：")
-
-        if choice == "1":
-            simple_llm_chain = SimpleLlmChain()
-            simple_llm_chain.ask()
+        llm_chain = create_chain(choice)
+        if llm_chain:
+            llm_chain.ask()
             break
-        elif choice == "2":
-            retrieval_llm_chain = RetrievalLLMChain()
-            retrieval_llm_chain.ask()
-            break
-        elif choice == "3":
-            agent_llm_chain = AgentLLMChain()
-            agent_llm_chain.ask()
-            break
-        else:
-            print("无效的选项，请重新选择。")
+        
